937af9b0af08fa54807c376311da1f2760e8a305
jcandrews2 <jcandrews2@icloud.com>
2022-09-10 22:20:16 -0600

    wordle-clone

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Board from "../components/Board.jsx"
+import Keyboard from "../components/Keyboard.jsx"
+//import global styles
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp() {
+  return (
+  <div className = "app"> 
+    <nav>
+      <h1>Wordle</h1> 
+    </nav> 
+    <Board /> 
+    <Keyboard /> 
+  </div>
+  );
 }
 
 export default MyApp
+
