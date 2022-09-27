8cadc99ba4996376ff6c557a8cf62f432eb0a3e8
jcandrews2 <jcandrews2@icloud.com>
2022-09-26 21:11:48 -0600

    Added global state for the board.

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,37 @@
-import Board from "../components/Board.jsx"
-import Keyboard from "../components/Keyboard.jsx"
-import "../styles/globals.css"
+import React, { useState, createContext, useEffect } from "react";
+import DefaultBoard from "../components/DefaultBoard.jsx";
+import Board from "../components/Board.jsx";
+import Keyboard from "../components/Keyboard.jsx";
+import "../styles/globals.css";
+
+export const AppContext = createContext();
 
 function MyApp() {
+  const [board, setBoard] = useState(DefaultBoard);
+  const [solution, setSolution] = useState(null);
+
   return (
-  <div className = "App"> 
-    <nav>
-      <h1>Wordle</h1> 
-    </nav> 
-    <Board /> 
-    <Keyboard /> 
-  </div>
+    <div className="App">
+      <div className="Nav">
+        <nav>
+          <div className="h1">
+            <h1>Wordle</h1>
+          </div>
+        </nav>
+      </div>
+      <AppContext.Provider
+        value={{
+          board,
+          setBoard,
+          solution,
+          setSolution,
+        }}
+      >
+        <Board />
+        <Keyboard />
+      </AppContext.Provider>
+    </div>
   );
 }
 
-export default MyApp
-
+export default MyApp;
